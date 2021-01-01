425325a1b13d359afc79a4f52528850835c69dcf
leanhtai01 <leanhtai01@gmail.com>
2021-01-01 02:22:34 +0000

    first commit

---
+++ post_install/configure_git.sh
@@ -0,0 +1,6 @@
+#!/usr/local/bin/bash
+
+set -e
+
+git config --global user.email "leanhtai01@gmail.com"
+git config --global user.name "leanhtai01"
---
+++ system_install/install_GNOME.sh
@@ -0,0 +1,5 @@
+#!/usr/local/bin/bash
+
+set -e
+
+pkg install baobab cheese eog evince file-roller gdm gnome-backgrounds gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-color-manager gnome-control-center gnome-keyring gnome-menus gnome-photos gnome-screenshot gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-system-monitor gnome-terminal gnome-themes-extra mutter nautilus sushi seahorse chrome-gnome-shell khelpcenter dconf-editor gnome-tweaks totem evolution evolution-ews tracker-miners
