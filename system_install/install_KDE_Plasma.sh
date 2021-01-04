910014c4c9a0606349cf0e64c4c7c5bdbd576f69
leanhtai01 <leanhtai01@gmail.com>
2021-01-04 12:35:18 +0700

    initial setup KDE Plasma

--- system_install/install_KDE_Plasma.sh
+++ system_install/install_KDE_Plasma.sh
@@ -2,4 +2,8 @@
 
 set -e
 
-pkg install -y plasma ark dolphin dolphin-plugins kate kleopatra konsole okular kdegraphics-thumbnailers digikam kdemultimedia-ffmpegthumbs spectacle gwenview gnome-keyring sddm
+pkg install -y plasma5-plasma ark dolphin dolphin-plugins kleopatra konsole okular kdegraphics-thumbnailers digikam kdemultimedia-ffmpegthumbs spectacle gwenview sddm khelpcenter
+
+sysrc dbus_enable=YES
+sysrc hald_enable=YES
+sysrc sddm_enable=YES
