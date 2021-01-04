47ba62c0b6849fdffb18412d2972f73b036888ed
leanhtai01 <leanhtai01@gmail.com>
2021-01-04 08:13:44 +0700

    add file install_KDE_Plasma.sh

---
+++ system_install/install_KDE_Plasma.sh
@@ -0,0 +1,5 @@
+#!/usr/local/bin/bash
+
+set -e
+
+pkg install -y plasma ark dolphin dolphin-plugins kate kleopatra konsole okular kdegraphics-thumbnailers digikam kdemultimedia-ffmpegthumbs spectacle gwenview gnome-keyring sddm
