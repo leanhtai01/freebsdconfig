63682cccb7cb860f655ea78b5d1abb0137bcc023
leanhtai01 <leanhtai01@gmail.com>
2021-01-04 14:23:43 +0700

    install drm-kmod for intel gpu

--- system_install/install_gpu_driver.sh
+++ system_install/install_gpu_driver.sh
@@ -4,4 +4,7 @@ set -e
 
 install_command="pkg install -y"
 
+(cd /usr/ports/graphics/drm-kmod; make install clean)
 $install_command intel-backlight intel-compute-runtime libva-intel-media-driver
+
+sysrc kld_list="i915kms"
