e9aa0a46ef4c8b9bfc97b899efe31e8b953fc1be
leanhtai01 <leanhtai01@gmail.com>
2021-09-07 16:40:51 +0700

    copy required config file i3

--- system_install/configure_i3.sh
+++ system_install/configure_i3.sh
@@ -2,4 +2,9 @@
 
 set -e
 
+# create required files and dirs
 mkdir -p /home/$(whoami)/.config
+
+# copy required files
+cp -r i3config/i3 /home/$(whoami)/.config
+cp -r i3config/i3status /home/$(whoami)/.config
