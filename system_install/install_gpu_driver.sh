0e40417915142cf6e34ea572b4732af5b9af383b
leanhtai01 <leanhtai01@gmail.com>
2021-09-07 16:12:36 +0700

    remove intel-backlight

--- system_install/install_gpu_driver.sh
+++ system_install/install_gpu_driver.sh
@@ -4,6 +4,6 @@ set -e
 
 install_command="pkg install -y"
 
-$install_command drm-kmod intel-backlight intel-compute-runtime libva-intel-media-driver
+$install_command drm-kmod intel-compute-runtime libva-intel-media-driver
 
 sysrc kld_list="i915kms"
