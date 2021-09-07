80eb82eb702070a8d4e61c2bb69ed1181eb9794a
leanhtai01 <leanhtai01@gmail.com>
2021-09-07 16:45:06 +0700

    add .Xresources file

--- system_install/configure_i3.sh
+++ system_install/configure_i3.sh
@@ -8,3 +8,4 @@ mkdir -p /home/$(whoami)/.config
 # copy required files
 cp -r i3config/i3 /home/$(whoami)/.config
 cp -r i3config/i3status /home/$(whoami)/.config
+cp data/.Xresources /home/$(whoami)
