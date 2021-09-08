fabe88438f5d332917321e30f445dbc02d202991
leanhtai01 <leanhtai01@gmail.com>
2021-09-08 10:01:09 +0700

    let user choose desktop environment to install

--- system_install/install_desktop_environment.sh
+++ system_install/install_desktop_environment.sh
@@ -10,3 +10,21 @@ $install_command xorg
 
 # add user to video groups
 pw group mod video -m leanhtai01
+
+if [ ! -z $1 ]
+then
+    for de in $1
+    do
+	case $de in
+	    GNOME)
+		printf 'GNOME will be installed!\n'
+		;;
+	    KDEPlasma)
+	        . $current_dir/install_KDE_Plasma.sh
+		;;
+	    i3)
+		.$current_dir/install_i3.sh
+		;;
+	esac
+    done
+fi
