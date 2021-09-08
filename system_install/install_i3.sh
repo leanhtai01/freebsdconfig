99abd78c9e16ec67f2f9fff698e211f6587638bf
leanhtai01 <leanhtai01@gmail.com>
2021-09-08 10:03:38 +0700

    i3: merge install and configure

--- system_install/install_i3.sh
+++ system_install/install_i3.sh
@@ -5,3 +5,11 @@ set -e
 install_command="pkg install -y"
 
 $install_command i3 i3lock i3status dmenu rxvt-unicode feh
+
+# create required files and dirs
+mkdir -p /home/$(whoami)/.config
+
+# copy required files
+cp -r i3config/i3 /home/$(whoami)/.config
+cp -r i3config/i3status /home/$(whoami)/.config
+cp data/.Xresources /home/$(whoami)
