3a27f28e72fce3a38f622aad51ed3fcb5f183715
leanhtai01 <leanhtai01@gmail.com>
2021-09-08 12:40:00 +0700

    configure git

--- system_install/install.sh
+++ system_install/install.sh
@@ -10,4 +10,5 @@ gpu=
 . $current_dir/set_install_info.sh
 . $current_dir/install_gpu_driver.sh $gpu
 . $current_dir/install_core_packages.sh
+. $current_dir/configure_git.sh
 . $current_dir/install_desktop_environment.sh "$desktop_environment" $username
