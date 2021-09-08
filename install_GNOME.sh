3b2de3121c9cb436dc9132738a746a888c27dce6
leanhtai01 <leanhtai01@gmail.com>
2021-09-08 14:38:35 +0700

    enable some services for GNOME

--- install_GNOME.sh
+++ install_GNOME.sh
@@ -5,3 +5,7 @@ set -e
 install_command="pkg install -y"
 
 $install_command baobab cheese eog evince file-roller gdm gnome-backgrounds gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-color-manager gnome-control-center gnome-keyring gnome-menus gnome-photos gnome-screenshot gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-system-monitor gnome-terminal gnome-themes-extra mutter nautilus sushi seahorse chrome-gnome-shell khelpcenter dconf-editor gnome-tweaks totem tracker-miners
+
+sysrc dbus_enable=YES
+sysrc gnome_enable=YES
+sysrc gdm_enable=YES
