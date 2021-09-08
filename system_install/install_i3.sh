caab5b2224fecc35faf7fbee2a24f73c5961eddf
leanhtai01 <leanhtai01@gmail.com>
2021-09-08 10:10:44 +0700

    i3: receive username from cli argument

--- system_install/install_i3.sh
+++ system_install/install_i3.sh
@@ -3,13 +3,18 @@
 set -e
 
 install_command="pkg install -y"
+username=$1
 
 $install_command i3 i3lock i3status dmenu rxvt-unicode feh
 
 # create required files and dirs
-mkdir -p /home/$(whoami)/.config
+mkdir -p /home/${username}/.config
 
 # copy required files
-cp -r i3config/i3 /home/$(whoami)/.config
-cp -r i3config/i3status /home/$(whoami)/.config
-cp data/.Xresources /home/$(whoami)
+cp -r i3config/i3 /home/${username}/.config
+cp -r i3config/i3status /home/${username}/.config
+cp data/.Xresources /home/${username}
+
+# change owner from root to user
+chown -R ${username}:${username} /home/${username}/.config
+chown -R ${username}:${username} /home/${username}/.Xresources
