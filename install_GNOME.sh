1fa0f5ec97454fce9370c4beaf7ad807accabf8b
leanhtai01 <leanhtai01@gmail.com>
2021-09-08 14:33:23 +0700

    not verify when install GNOME

--- install_GNOME.sh
+++ install_GNOME.sh
@@ -2,4 +2,6 @@
 
 set -e
 
-pkg install baobab cheese eog evince file-roller gdm gnome-backgrounds gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-color-manager gnome-control-center gnome-keyring gnome-menus gnome-photos gnome-screenshot gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-system-monitor gnome-terminal gnome-themes-extra mutter nautilus sushi seahorse chrome-gnome-shell khelpcenter dconf-editor gnome-tweaks totem evolution evolution-ews tracker-miners
+install_command="pkg install -y"
+
+$install_command baobab cheese eog evince file-roller gdm gnome-backgrounds gnome-calculator gnome-calendar gnome-characters gnome-clocks gnome-color-manager gnome-control-center gnome-keyring gnome-menus gnome-photos gnome-screenshot gnome-session gnome-settings-daemon gnome-shell gnome-shell-extensions gnome-system-monitor gnome-terminal gnome-themes-extra mutter nautilus sushi seahorse chrome-gnome-shell khelpcenter dconf-editor gnome-tweaks totem tracker-miners
