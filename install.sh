77bf6e6a4681afe8ee380972464505828c343cca
leanhtai01 <leanhtai01@gmail.com>
2021-09-08 13:37:29 +0700

    not sourcing un-needed scripts

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ desktop_environment=
 gpu=
 
 . $current_dir/set_install_info.sh
-. $current_dir/install_gpu_driver.sh $gpu
-. $current_dir/install_core_packages.sh
-. $current_dir/configure_git.sh
-. $current_dir/install_desktop_environment.sh "$desktop_environment" "$username"
+$current_dir/install_gpu_driver.sh $gpu
+$current_dir/install_core_packages.sh
+$current_dir/configure_git.sh
+$current_dir/install_desktop_environment.sh "$desktop_environment" "$username"
